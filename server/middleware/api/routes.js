ce5fd6a2f143398907176fab03cc5c81ea5c881c
Yusuf Modan <yumodan@gmail.com>
2016-03-09 01:55:01 -0800

    [feat] intro to koa generators

--- server/middleware/api/routes.js
+++ server/middleware/api/routes.js
@@ -2,5 +2,7 @@ const router = require('koa-router')();
 const userRoutes = require('./userRoutes');
 
 router.use('/users', userRoutes.routes());
+// put more routes here
+// create more routers
 
 module.exports = router;
--- server/middleware/api/userRoutes.js
+++ server/middleware/api/userRoutes.js
@@ -1,5 +1,24 @@
 const userRoutes = require('koa-router')();
 
-userRoutes.get('/', function *() { });
+function test() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      console.log('hellowworldddd');
+      resolve('hellowworldddd');
+    }, 100);
+  });
+}
+
+userRoutes.get('/', function *() {
+  try {
+    const str = yield test();
+    console.log(`${str}man i hope this works`);
+  } catch (errrr) {
+    console.log(errrr);
+  }
+});
+
+
+// the above function works I simply removed the console.log to make it look cleaner
 
 module.exports = userRoutes;
